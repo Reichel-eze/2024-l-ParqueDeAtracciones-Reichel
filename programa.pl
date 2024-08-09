3b925c68866949b5c3fb69f0ae291213fda98306
Reichel-eze <reicheleze@gmail.com>
2024-08-09 19:11:06 -0300

    Arreglo Problemas y hago hasta el 3) inclusive

--- programa.pl
+++ programa.pl
@@ -1,9 +1,23 @@
 % Parque de Atracciones
 
 persona(nina, joven, 22, 1.60).
-persona(marcos, nino, 8, 1.32).
+persona(marcos, ninio, 8, 1.32).
 persona(osvaldo, adolescente, 13, 1.29).
+persona(ana, adolescente, 13, 1.6).
 
+% Estos ejemplos son arbitrarios, podríamos ampliar los límites, 
+% agregar otros grupos etarios 
+% o incluso solapar los intervalos porque no es necesario que sean excluyentes
+grupoEtario(ninio,0,12).
+grupoEtario(adolescente,13,18). 
+grupoEtario(joven,19,30).
+grupoEtario(adulto,30,100).
+
+perteneceAGrupoEtario(Persona, GrupoEtario) :-
+    persona(Persona, GrupoEtario, Edad, _),
+    grupoEtario(GrupoEtario, E1, E2),
+    between(E1, E2, Edad).
+    
 %atraccion(trenFantasma(12)).
 %atraccion(montanaRusa(1.30)).
 %atraccion(maquinaTiquetera).
@@ -17,6 +31,12 @@ atraccion(maquinaTiquetera, edad(0), altura(0)).
 atraccion(toboganGigante, edad(0), altura(1.50)).
 atraccion(rioLento, edad(0), altura(0)).
 atraccion(piscinaDeOlas, edad(5), altura(0)).
+
+requisito(trenFantasma, edad(12)).
+requisito(montanaRusa, altura(1.30)).
+requisito(toboganGigante, altura(1.50)).
+requisito(piscinaDeOlas, edad(5)).
+% los parques de atracciones que no tienen requesitos no se ponen!!
 
 tiene(parqueDeLaCosta, trenFantasma).
 tiene(parqueDeLaCosta, montanaRusa).
@@ -34,6 +54,32 @@ puedeSubir(Persona, Atraccion) :-
     atraccion(Atraccion, edad(EdadMinima), altura(AlturaMinima)),
     EdadPersona >= EdadMinima,
     AlturaPersona >= AlturaMinima.
+
+puedeSubirV2(Persona, Atraccion) :-     % PARA LAS ATRACCIONES QUE TIENEN REQUISITOS
+    persona(Persona, _, _, _),  % existe la persona
+    requisito(Atraccion, Requisito),    % existe la atraccion con requisitos
+    cumpleRequisito(Requisito, Persona).
+
+puedeSubirV2(Persona, Atraccion) :-    % PARA LAS ATRACCIONES QUE NO TIENEN REQUISITOS
+    persona(Persona, _, _, _),
+    tiene(_, Atraccion),            % existe un Parque que tiene a la Atraccion
+    not(requisito(Atraccion, _)).   % PERO la atraccion NO tiene requisitos
+
+%Asumiendo que una atracción puede tener varios requisitos, la misma regla ya considera cuando no tiene requisitos
+
+puedeSubirV2(Persona, Atraccion) :-     
+    persona(Persona, _, _, _),
+    tiene(_, Atraccion),
+    forall(requisito(Atraccion, Requisito), cumpleRequisito(Requisito, Persona)).
+    % PARA TODO requesito de la atraccion, la persona cumple los requisitos!!
+
+cumpleRequisito(edad(EdadMinima), Persona) :-
+    persona(Persona, _, Edad, _),
+    EdadMinima =< Edad.
+
+cumpleRequisito(altura(AlturaMinima), Persona) :-
+    persona(Persona, _, _, Altura),
+    AlturaMinima =< Altura.
 
 %cumpleRequisito(trenFantasma(12), persona(_, _, Edad, _)) :- Edad >= 12.
 %cumpleRequisito(montanaRusa(1.30), persona(_,_,_,Altura)) :- Altura >= 1.30.
@@ -59,16 +105,21 @@ esParaElle(Persona, Parque) :-
 % 3) malaIdea/2, relaciona un grupo etario (adolescente/niño/joven/adulto/etc) con un parque, y nos dice que "es mala idea" que las personas de ese 
 % grupo vayan juntas a ese parque, si es que no hay ningún juego al que puedan subir todos
 
-% --> si no existe ningún juego al que puedan subir todos, entonces, not(forall) --> hay por lo menos un jugador que no puede subir
+% --> si no existe ningún juego al que puedan subir todos, entonces, not(forall) --> NO EXISTE ALGUNA ATRACCION
 
 malaIdea(GrupoEtario, Parque) :-
-    grupoEtario(GrupoEtario),
+    grupoEtario(GrupoEtario, _, _),
     tiene(Parque, _),
-    not(forall(persona(Persona,GrupoEtario,_,_), (tiene(Parque,Atraccion), puedeSubir(Persona, Atraccion)))).
+    not(hayUnaAtraccionParaTodes(GrupoEtario, Parque)).     % NO EXISTE UNA ATRACCION PARA TODES!!
 
-malaIdeaV2(GrupoEtario, Parque) :-
-    persona(Persona, GrupoEtario, _, _),
-    tiene(Parque, _),
-    not((tiene(Parque,Atraccion), puedeSubir(Persona, Atraccion))).
+hayUnaAtraccionParaTodes(Grupo, Parque) :-
+    tiene(Parque, Atraccion),
+    forall(perteneceAGrupoEtario(Persona, Grupo), puedeSubirV2(Persona, Atraccion)).
+    % PARA TODOS LAS PERSONAS QUE FORMAN PARTEN DEL GRUPO, las mismas pueden subir a la atraccion
+        
+%malaIdeaV2(GrupoEtario, Parque) :-
+%    persona(Persona, GrupoEtario, _, _),
+%    tiene(Parque, _),
+%    not((tiene(Parque,Atraccion), puedeSubir(Persona, Atraccion))).
 
-grupoEtario(Grupo) :- persona(_, Grupo, _, _).
+
